8b02226f2b036089ebb67f7f183002b945de96e1
David come <david@localhost.localdomain>
2013-12-30 15:42:46 +0100

    Add CMakeList and some test file for the software

---
+++ test/header.hpp
@@ -0,0 +1,61 @@
+#include <string>
+namespace AA
+{
+  namespace BB
+  {
+    struct D{
+	
+      struct A{};
+      class C
+      {
+      public :
+        //should be found
+        C();
+        ~C();	
+        void a() noexcept ;
+        void a(int) throw(A,double);
+
+        //nope, deleted
+        C& operator=(const C&) = delete;
+
+        //nope, default
+        C(const C&c)=default;
+
+        operator A();
+  
+        void Z(std::string s="foobar");
+  
+        //member
+        static const int x;
+
+	static int foobarz();
+
+        //nope, has a body
+        const double fct1(double chose = 5.){return 5.;}
+
+        //yes, not pure
+        virtual const A fct3(const float& truc=5.) const;
+
+        //nope, pure
+        virtual const A fct2(const float& truc) const =0;
+
+        //should not be and is not !!
+        void foo() const;	
+
+        //template, so no
+        template <class T> void bar();
+	}; //-- C 
+		
+    }; //-- D 
+  } //-- BB
+} //-- AA 
+/*
+struct D
+{
+  struct E
+  {
+    void EE(AA::BB::D::A& a);
+  };
+};
+
+*/
---
+++ test/test.cpp
@@ -0,0 +1,46 @@
+#include "header.hpp"
+namespace AA {
+namespace BB {
+ D::C::C() 
+{
+}
+
+ D::C::~C() 
+{
+}
+
+void D::C::a()  noexcept 
+{
+}
+
+void D::C::a(int ) throw (struct AA::BB::D::A,double)
+{
+}
+
+ D::C::operator A() 
+{
+}
+
+void D::C::Z(std::string s /* "foobar" */) 
+{
+}
+
+int D::C::foobarz() 
+{
+}
+
+const struct AA::BB::D::A D::C::fct3(const float & truc /* 5. */)  const 
+{
+}
+
+void D::C::foo()  const 
+{
+}
+
+}
+}
+const int AA::BB::D::C::x;
+namespace AA {
+namespace BB {
+}
+}
